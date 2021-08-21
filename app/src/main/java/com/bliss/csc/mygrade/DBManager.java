71f88169e2712f3a0c71367f08a389197ff622e0
DoJiSeon <djs6620@gmail.com>
2021-08-21 23:50:30 +0900

    코드 주석 추가

--- app/src/main/java/com/bliss/csc/mygrade/DBManager.java
+++ app/src/main/java/com/bliss/csc/mygrade/DBManager.java
@@ -10,18 +10,17 @@ import androidx.annotation.Nullable;
 
 public class DBManager extends SQLiteOpenHelper {
 
-    private static final String TABLE_COLUMN_AVERAGE = "average";
-    private static final String TABLE_NAME = "MyGrade";
-    public static final String DB_NAME = "dbtest.db";
-    public static final int DB_VERSION = 1;
-    private SQLiteDatabase mDB;
-    private int count_ID = 1;
-
-    public static final String COL_1 = "_id";
-    public static final String COL_2 = "grade";
-    public static final String COL_3 = "semester";
-    public static final String COL_4 = "exam";
-    public static final String COL_5 = "average";
+    private static final String TABLE_COLUMN_AVERAGE = "average"; // 평균 필드의 이름 선언
+    private static final String TABLE_NAME = "MyGrade"; // 테이블 이름 선언
+    public static final String DB_NAME = "dbtest.db"; // db 이름 선언
+    public static final int DB_VERSION = 1; //데이터 베이스의 버전
+    private SQLiteDatabase mDB; // mDB 선언
+    // *표 = 테이블, 행 = 레코드, 열 = 필드 라고 함.*
+    public static final String COL_1 = "_id"; // 첫 번째 필드(열, 세로)의 이름
+    public static final String COL_2 = "grade";// 두 번째 필드(열, 세로)의 이름
+    public static final String COL_3 = "semester";// 세 번째 필드(열, 세로)의 이름
+    public static final String COL_4 = "exam";// 네 번째 필드(열, 세로)의 이름
+    public static final String COL_5 = "average";// 다섯 번째 필드(열, 세로)의 이름
 
     public DBManager(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -29,51 +28,53 @@ public class DBManager extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        // 테이블을 만든다. 테이블의 필드 갯수 설정.
         db.execSQL("CREATE TABLE MyGrade( _id INTEGER ," + "grade INTEGER, semester INTEGER, exam TEXT, average INTEGER);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS MyGrade");
-        onCreate(db);
+        db.execSQL("DROP TABLE IF EXISTS MyGrade"); // 테이블의 버전 업그레이드 혹은 테이블 수정 시 기존에 있던 테이블이 있다면 삭제
+        onCreate(db); // 재생성
     }
 
     @Override public void onOpen(SQLiteDatabase db) { super.onOpen(db); mDB = db; }
 
+    // 값 입력 메서드
     public void insert(DBManager mDBManager, Integer Id ,Integer grade, Integer semester, String exam, Integer average)
     {
-        mDBManager.getWritableDatabase();
-        mDB.execSQL("INSERT INTO MyGrade VALUES (" + Id + ", "+ grade +", " +semester+", '"+ exam +"', "+ average +" );");
-        mDBManager.close();
-        String insert = Id+ "," + grade + " ,"+ semester+" ," + exam + ", " + average;
-        Log.i("Insert DB : ", insert);
+        mDBManager.getWritableDatabase(); // mDBManager의 데이터베이스를 읽고 쓰는 형식으로 열어줌.
+        mDB.execSQL("INSERT INTO MyGrade VALUES (" + Id + ", "+ grade +", " +semester+", '"+ exam +"', "+ average +" );"); // 받아온 파라미터 값 넣어줌.
+        mDBManager.close(); // 데이터 베이스 사용이 끝났으므로 닫아줌
+        String insert = Id+ "," + grade + " ,"+ semester+" ," + exam + ", " + average; // insert변수에 값들을 넣어줌
+        Log.i("Insert DB : ", insert); // 값들을 로그로 확인
     }
 
+    // 값 삭제 메서드
     public void delete(DBManager mDBManager)
     {
-        mDBManager.getWritableDatabase();
-        mDB.execSQL("DELETE FROM MyGrade");
-        mDBManager.close();
+        mDBManager.getWritableDatabase(); // mDBManager의 데이터베이스를 읽고 쓰는 형식으로 열어줌.
+        mDB.execSQL("DELETE FROM MyGrade"); // MyGrade에 있는 모든 테이블을 삭제함.
+        mDBManager.close(); // 데이터 베이스 사용이 끝났으므로 닫아줌
         Log.i("Delete DB", "모든 테이블이 삭제 되었습니다.");
     }
 
+    // 값 업데이트 메서드
     public void update(DBManager mDBManager, Integer id, Integer grade, Integer semester, String exam, Integer average)
     {
-        mDBManager.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-
-        contentValues.put(COL_1, id);
-        contentValues.put(COL_2, grade);
-        contentValues.put(COL_3, semester);
-        contentValues.put(COL_4, exam);
-        contentValues.put(COL_5, average);
-        mDB.update(TABLE_NAME,contentValues,"_id = ?", new String[]{String.valueOf(id)});
-
-        // mDB.execSQL("INSERT OR REPLACE INTO MyGrade(_id, grade, semester, exam, average) VALUES (" + id+ ", "+ grade +", " +semester+", '"+ exam +"', "+ average +" );");
-        mDBManager.close();
-        Log.v(null,"Update DB : "+id);
+        mDBManager.getWritableDatabase(); // mDBManager의 DB를 읽고 쓸 수 있는 형식으로 받아옴.
+        ContentValues contentValues = new ContentValues(); // contentValues 초기화
+        contentValues.put(COL_1, id); // 첫번째 필드의 값 바꿔줌
+        contentValues.put(COL_2, grade); // 두번째 필드의 값 바꿔줌
+        contentValues.put(COL_3, semester); // 세번째 필드의 값 바꿔줌
+        contentValues.put(COL_4, exam); // 네번째 필드의 값 바꿔줌
+        contentValues.put(COL_5, average); // 다섯번째 필드의 값 바꿔줌
+        mDB.update(TABLE_NAME,contentValues,"_id = ?", new String[]{String.valueOf(id)}); // 받아온 id 값이 있는 레코드의 값들을 바꿈
+        mDBManager.close(); // 다 사용했으므로 닫아줌.
+        Log.i(null,"Update DB : "+id);
     }
 
+    // 평균 값 계산 메서드 (아직 개발중...)
     public void all_average(DBManager mDBManager) {
         String query = "SELECT AVG("+ TABLE_COLUMN_AVERAGE +") FROM "+ TABLE_NAME;
         mDB.rawQuery(query, null);
--- app/src/main/java/com/bliss/csc/mygrade/Data.java
+++ app/src/main/java/com/bliss/csc/mygrade/Data.java
@@ -30,31 +30,32 @@ public class Data extends Activity {
     EditText grade, semester, average, id;
     ListView listView, listView2, listView3, listView4, listView_id;
     Cursor cursor;
-    ImageButton btn_help;
-    ArrayAdapter adapter, adapter2, adapter3, adapter4, adapter_id;
-    RadioGroup examgroup;
+    ImageButton btn_help; // 경고창
+    ArrayAdapter adapter, adapter2, adapter3, adapter4, adapter_id; //리스트에 넣어줄 어댑터
+    RadioGroup examgroup; //라디오 그룹
 
     SharedPreferences pref; //프리퍼런스
     SharedPreferences.Editor editor; //에디터
 
-    int countInt;
+    int countInt; //데이터베이스에 넘길 번호 값
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
 
-        grade = (EditText)findViewById(R.id.grade);
-        semester = (EditText)findViewById(R.id.semester);
-        average = (EditText)findViewById(R.id.average);
-        id = (EditText)findViewById(R.id.id_edt);
+        grade = (EditText)findViewById(R.id.grade); //학년 받아올 에디트 텍스트
+        semester = (EditText)findViewById(R.id.semester);//학기 받아올 에디트 텍스트
+        average = (EditText)findViewById(R.id.average);//평균 받아올 에디트 텍스트
+        id = (EditText)findViewById(R.id.id_edt);//번호 받아올 에디트 텍스트
 
-        btn_insert = (Button)findViewById(R.id.btn_insert);
-        btn_delete = (Button)findViewById(R.id.btn_delete);
-        btn_update = (Button)findViewById(R.id.btn_update);
-        btn_select = (Button)findViewById(R.id.btn_select);
-        btn_help = (ImageButton) findViewById(R.id.btn_help);
+        btn_insert = (Button)findViewById(R.id.btn_insert); // 값 입력 버튼
+        btn_delete = (Button)findViewById(R.id.btn_delete); // 값 삭제 버튼
+        btn_update = (Button)findViewById(R.id.btn_update); // 값 업데이트 버튼
+        btn_select = (Button)findViewById(R.id.btn_select); // 값 조회 버튼
+        btn_help = (ImageButton) findViewById(R.id.btn_help); // 사용 안내 버튼
 
+        //리스트 뷰들
         listView = (ListView)findViewById(R.id.listview);
         listView2 = (ListView)findViewById(R.id.listview2);
         listView3 = (ListView)findViewById(R.id.listview3);
@@ -62,46 +63,49 @@ public class Data extends Activity {
         listView_id = (ListView)findViewById(R.id.listview_id);
 
 
-        examgroup = (RadioGroup)findViewById(R.id.examRgroup);
+        examgroup = (RadioGroup)findViewById(R.id.examRgroup); //라디오 그룹
 
-        mDBManager = new DBManager(this);
-        mDBManager.getReadableDatabase();
-        mDBManager.getWritableDatabase();
-        mDBManager.close();
+        mDBManager = new DBManager(this); //DB매니저 인스턴스화
+        mDBManager.getReadableDatabase(); // 데이터 베이스를 읽어올수만 있음.
+        mDBManager.getWritableDatabase(); // 데이터 베이스를 읽고, 쓸 수 있음.
+        mDBManager.close(); // 데이터 베이스 닫기
 
-        pref = getSharedPreferences("pref",Activity.MODE_PRIVATE);
-        editor = pref.edit();
+        pref = getSharedPreferences("pref",Activity.MODE_PRIVATE); // 값 영구적으로 저장하는 데에 쓰임
+        editor = pref.edit(); // 선언
 
-        countInt = pref.getInt("myCount",1);
+        countInt = pref.getInt("myCount",1); // myCount라는 키에서 값을 받아오고, 값이 없다면 1을 넣어줌.
 
+        // 입력 버튼을 눌렀을 때
         btn_insert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
                     try {
                         try{
-                            Integer Grade = Integer.valueOf(grade.getText().toString());
-                            Integer Semester = Integer.valueOf(semester.getText().toString());
-                            Integer Average = Integer.valueOf(average.getText().toString());
+                            Integer Grade = Integer.valueOf(grade.getText().toString()); // 학년 에디트 텍스트에 쓰여진 값을 받아서 변수에 넣음.
+                            Integer Semester = Integer.valueOf(semester.getText().toString());// 학기 에디트 텍스트에 쓰여진 값을 받아서 변수에 넣음.
+                            Integer Average = Integer.valueOf(average.getText().toString());// 평균 에디트 텍스트에 쓰여진 값을 받아서 변수에 넣음.
 
                             String exam;
-                            int id = examgroup.getCheckedRadioButtonId();
-                            RadioButton rb = (RadioButton)findViewById(id);
-                            exam = rb.getText().toString();
+                            int id = examgroup.getCheckedRadioButtonId(); // examgroup의 체크된 라디오 버튼의 아이디를 받아옴.
+                            RadioButton rb = (RadioButton)findViewById(id); // 받아온 아이디를 rb 라디오 버튼에 넣어줌.
+                            exam = rb.getText().toString(); // rb 라디오 버튼의 텍스트를 받아옴. ex)중간고사, 기말고사
 
+                            //mDBManager의 insert 메소드로 파라미터 값들을 보내줌.
                             mDBManager.insert(mDBManager, countInt , Grade, Semester, exam, Average);
 
+                            // 학년 값을 넣어줬으므로 이제 필요없는 값 지워줌.
                             grade.setText("");
                             semester.setText("");
                             average.setText("");
 
-                            countInt = countInt +1;
-                            editor.putInt("myCount", countInt);
-                            editor.apply();
-                        }catch (NullPointerException e){
+                            countInt = countInt +1; // 값 저장 후 1을 추가, 처음 값을 넣었다면 다음 번호로는 1이 추가되어 2를 값으로 보냄.
+                            editor.putInt("myCount", countInt); // 추가한 값 저장.
+                            editor.apply(); // apply를 써주지 않는다면 영구적으로 저장되지 않음. 꼭 쓰기
+                        }catch (NullPointerException e){ // 버튼안이 비어있을 때 나오는 예외에 대처하는 코드
                             Toast.makeText(getApplicationContext(),"모든 칸과 버튼을 채워주세요",Toast.LENGTH_SHORT).show();
                         }
-                    }catch (SQLiteConstraintException e){
+                    }catch (SQLiteConstraintException e){ // sqlite 예외에 대처하는 코드
                         Toast.makeText(getApplicationContext(),"시험 종류를 선택해주세요",Toast.LENGTH_SHORT).show();
                     }
 
@@ -112,49 +116,54 @@ public class Data extends Activity {
             }
         });
 
+
+        //삭제 버튼을 눌렀을 때
         btn_delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mDBManager.delete(mDBManager);
-                Toast.makeText(getApplicationContext(),"모든 데이터가 삭제되었습니다.",Toast.LENGTH_SHORT).show();
+                mDBManager.delete(mDBManager); // mDBManager에 현재 매니저를 보내줌.
+                Toast.makeText(getApplicationContext(),"모든 데이터가 삭제되었습니다.",Toast.LENGTH_SHORT).show(); // 알림창을 띄움.
 
-                countInt = 1;
-                editor.putInt("myCount", countInt);
+                countInt = 1; // 번호를 1로 초기화 해준다.
+                editor.putInt("myCount", countInt); // 초기화된 값을 저장해준다.
                 editor.apply();
             }
 
         });
 
+        // 업데이트 버튼을 눌렀을 때
         btn_update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 try {
-                    Integer ID = Integer.valueOf(id.getText().toString());
-                    Integer Grade = Integer.valueOf(grade.getText().toString());
-                    Integer Semester = Integer.valueOf(semester.getText().toString());
-                    Integer Average = Integer.valueOf(average.getText().toString());
-                    String exam;
-                    int id = examgroup.getCheckedRadioButtonId();
-                    RadioButton rb = (RadioButton)findViewById(id);
-                    exam = rb.getText().toString();
-                    mDBManager.update(mDBManager, ID, Grade, Semester, exam , Average);
+                    Integer ID = Integer.valueOf(id.getText().toString()); // 번호 에디트 텍스트에 쓰여진 값을 받아서 변수에 넣음.
+                    Integer Grade = Integer.valueOf(grade.getText().toString()); // 학년 에디트 텍스트에 쓰여진 값을 받아서 변수에 넣음.
+                    Integer Semester = Integer.valueOf(semester.getText().toString()); // 학기 에디트 텍스트에 쓰여진 값을 받아서 변수에 넣음.
+                    Integer Average = Integer.valueOf(average.getText().toString()); // 평균 에디트 텍스트에 쓰여진 값을 받아서 변수에 넣음.
+                    String exam; //
+                    int id = examgroup.getCheckedRadioButtonId(); // examgroup의 체크된 라디오 버튼의 아이디를 받아옴.
+                    RadioButton rb = (RadioButton)findViewById(id); // 받아온 아이디를 rb 라디오 버튼에 넣어줌.
+                    exam = rb.getText().toString();  // rb 라디오 버튼의 텍스트를 받아옴. ex)중간고사, 기말고사
+                    mDBManager.update(mDBManager, ID, Grade, Semester, exam , Average); // 받아 온 값들을 update 메소드로 넘겨줌
 
 
-                }catch (NumberFormatException e){
+                }catch (NumberFormatException e){ // 모든 버튼이 채워지지 않았을 때
                     Toast.makeText(getApplicationContext(), "수정하고 싶다면 모든 값을 채워주세요.",Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
+        // 조회 버튼을 눌렀을 때
         btn_select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //mDBManager.select(mDBManager);
+                //리스트 업데이트 데이터 실행
                 listUpdate();
             }
         });
 
+        // 경고창 이미지 버튼 클릭 시
         btn_help.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -166,10 +175,12 @@ public class Data extends Activity {
 
     private void showHelping_window() {
         AlertDialog.Builder msgBuilder = new AlertDialog.Builder(Data.this)
-                .setTitle("나의 성적 입력 가이드")
+                .setTitle("나의 성적 입력 가이드")// 경고창 제목
+                //경고창 내용 부분
                 .setMessage("1. 정보 입력 시 번호란을 제외한 나머지 칸(버튼)을 모두 입력/클릭합니다." +
                         " \n" +"2. 삭제 버튼 클릭 혹은 앱 삭제 시 모든 데이터가 삭제 됩니다.\n" +
                         "3. 정보 수정 시 수정할 리스트의 번호와 수정할 데이터를 입력하되 바뀌지 않은 값은 칸을 비우지 않고 그대로 채워 넣어준 후 업데이트 버튼을 누릅니다. ")
+                //확인 버튼
                 .setPositiveButton("확인", new DialogInterface.OnClickListener() {
                     @Override public void onClick(DialogInterface dialogInterface, int i) {
 
@@ -180,6 +191,7 @@ public class Data extends Activity {
 
     }
 
+    // 현재 테스트 중
     public Cursor avg_average(){
         //String query = "SELECT AVG("+ TABLE_COLUMN_AVERAGE +") FROM "+ TABLE_NAME;
         mDB = mDBManager.getReadableDatabase();
@@ -190,30 +202,31 @@ public class Data extends Activity {
 
     }
 
+    // 데이터 베이스 값들을 가져옴
     public void listUpdate(){
         mDB = mDBManager.getReadableDatabase();
-        cursor = mDB.rawQuery("SELECT * FROM MyGrade",null);
+        cursor = mDB.rawQuery("SELECT * FROM MyGrade",null); // MyGrade 테이블에서 모든 값들을 받아옴.
         startManagingCursor(cursor);
 
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
-        adapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
-        adapter3 = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
-        adapter4 = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
-        adapter_id = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1); // 학년 어댑터
+        adapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1);// 학기 어댑터
+        adapter3 = new ArrayAdapter(this, android.R.layout.simple_list_item_1);// 시험 어댑터
+        adapter4 = new ArrayAdapter(this, android.R.layout.simple_list_item_1);// 평균 어댑터
+        adapter_id = new ArrayAdapter(this, android.R.layout.simple_list_item_1);// 번호 어댑터
 
-        while (cursor.moveToNext()){
-            adapter_id.add(cursor.getInt(0));
-            adapter.add(cursor.getInt(1));
-            adapter2.add(cursor.getInt(2));
-            adapter3.add(cursor.getString(3));
-            adapter4.add(cursor.getInt(4));
+        while (cursor.moveToNext()){ // 커서가 현재 레코드의 끝까지 가면 다음 레코드로 이동
+            adapter_id.add(cursor.getInt(0)); // 첫번째 필드의 값
+            adapter.add(cursor.getInt(1)); // 두번째 필드의 값
+            adapter2.add(cursor.getInt(2)); //세번째 필드의 값
+            adapter3.add(cursor.getString(3)); // 네번째 필드의 값
+            adapter4.add(cursor.getInt(4)); //다섯번째 필드의 값
         }
-        listView_id.setAdapter(adapter_id);
-        listView.setAdapter(adapter);
-        listView2.setAdapter(adapter2);
-        listView3.setAdapter(adapter3);
-        listView4.setAdapter(adapter4);
-        cursor.close();
+        listView_id.setAdapter(adapter_id); // 번호 값을 넣을 리스트 뷰에 번호 어댑터 값을 넘겨 줌.
+        listView.setAdapter(adapter); // 학년 값을 넣을 리스트 뷰에 학년 어댑터 값을 넘겨 줌.
+        listView2.setAdapter(adapter2); // 학기 값을 넣을 리스트 뷰에 학기 어댑터 값을 넘겨 줌.
+        listView3.setAdapter(adapter3); // 시험 값을 넣을 리스트 뷰에 시험 어댑터 값을 넘겨 줌.
+        listView4.setAdapter(adapter4); // 평균 값을 넣을 리스트 뷰에 평균 어댑터 값을 넘겨 줌.
+        cursor.close(); // 커서를 다 이용했으므로 커서를 닫아줌.
     }
 
 
